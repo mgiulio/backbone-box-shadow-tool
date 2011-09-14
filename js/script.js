04c4495ae85997f7737228f12115f7159d25826f
mgiulio <giulio.mainardi@gmail.com>
2011-09-14 16:12:58 +0200

    Don't attach vars to the window object

--- js/script.js
+++ js/script.js
@@ -1,85 +1,82 @@
 $(function() {
 
-window.BoxShadow = Backbone.Model.extend({
-	defaults: function() {
-		return {
-			horizontalOffset: 0, 
-			verticalOffset: 0,
-			blurRadius: 0,
-			spreadDistance: 0,
-			color: 'rgba(0,0,0,0.5)'
-		};
-	},
-	getCSSText: function() {
-		var v = this.attributes.horizontalOffset + 'px ' +
-			this.attributes.verticalOffset + 'px ' +
-			this.attributes.blurRadius + 'px ' +
-			this.attributes.spreadDistance + 'px ' +
-			this.attributes.color + ';\n';
-		return '-moz-box-shadow: ' + v + '-webkit-box-shadow: ' + v + 'box-shadow: ' + v;
-	}
-});
+var
+	BoxShadowModel = Backbone.Model.extend({
+		defaults: function() {
+			return {
+				horizontalOffset: 0, 
+				verticalOffset: 0,
+				blurRadius: 0,
+				spreadDistance: 0,
+				color: 'rgba(0,0,0,0.5)'
+			};
+		},
+		getCSSText: function() {
+			var v = this.attributes.horizontalOffset + 'px ' +
+				this.attributes.verticalOffset + 'px ' +
+				this.attributes.blurRadius + 'px ' +
+				this.attributes.spreadDistance + 'px ' +
+				this.attributes.color + ';\n';
+			return '-moz-box-shadow: ' + v + '-webkit-box-shadow: ' + v + 'box-shadow: ' + v;
+		}
+	}),
+	SampleView = Backbone.View.extend({
+		el: $('#sample-box').get(0),
+		initialize: function() {
+			this.render();
+			this.model.bind('change', this.render, this);
+		},
+		render: function() {
+			this.el.style.cssText = this.model.getCSSText();
+			return this;
+		}
+	}),
+	OutputView = Backbone.View.extend({
+		el: $('#css-output'),
+		initialize: function() {
+			this.render();
+			this.model.bind('change', this.render, this);
+		},
+		render: function() {
+			this.el.html(this.model.getCSSText());
+			return this;
+		}
+	}),
+	ControlView = Backbone.View.extend({
+		el: $('#controls'),
+		events: {
+			'change': 'onChange'
+		},
+		onChange: function(e) {
+			var o = {};
+			o[e.target.id] = e.target.value;
+			this.model.set(o);
+		},
+		initialize: function() {
+			this.$('#horizontalOffset').val(this.model.get('horizontalOffset'));
+			this.$('#verticalOffset').val(this.model.get('verticalOffset'));
+			this.$('#verticalOffset').val(this.model.get('verticalOffset'));
+			this.$('#blurRadius').val(this.model.get('blurRadius'));
+			this.$('#spreadDistance').val(this.model.get('spreadDistance'));
+			this.$('#color').val(this.model.get('color'));
+		}
+	}),
+	AppView = Backbone.View.extend({
+		el: $('.widget.box-shadow'),
+		initialize: function() {
+			var boxShadow = new BoxShadowModel({
+				horizontalOffset: 5,
+				verticalOffset: 5,
+				blurRadius: 5,
+				spreadDistance: 3
+			});
+			this.sampleView = new SampleView({model: boxShadow});
+			this.outputView = new OutputView({model: boxShadow});
+			this.controlView = new ControlView({model: boxShadow});
+		 }
+	})
+;
 
-window.SampleView = Backbone.View.extend({
-	el: $('#sample-box').get(0),
-	initialize: function() {
-		this.render();
-		this.model.bind('change', this.render, this);
-	},
-	render: function() {
-		this.el.style.cssText = this.model.getCSSText();
-		return this;
-	}
-});
-
-window.OutputView = Backbone.View.extend({
-	el: $('#css-output'),
-	initialize: function() {
-		this.render();
-		this.model.bind('change', this.render, this);
-	},
-	render: function() {
-		this.el.html(this.model.getCSSText());
-		return this;
-	}
-});
-
-window.ControlView = Backbone.View.extend({
-	el: $('#controls'),
-	events: {
-		'change': 'onChange'
-	},
-	onChange: function(e) {
-		var o = {};
-		o[e.target.id] = e.target.value;
-		this.model.set(o);
-	},
-	initialize: function() {
-		this.$('#horizontalOffset').val(this.model.get('horizontalOffset'));
-		this.$('#verticalOffset').val(this.model.get('verticalOffset'));
-		this.$('#verticalOffset').val(this.model.get('verticalOffset'));
-		this.$('#blurRadius').val(this.model.get('blurRadius'));
-		this.$('#spreadDistance').val(this.model.get('spreadDistance'));
-		this.$('#color').val(this.model.get('color'));
-	},
-});
-
-window.AppView = Backbone.View.extend({
-	el: $('.widget.box-shadow'),
-	initialize: function() {
-		var boxShadow = new BoxShadow({
-			horizontalOffset: 5,
-			verticalOffset: 5,
-			blurRadius: 5,
-			spreadDistance: 3
-		});
-		
-		this.sampleView = new SampleView({model: boxShadow});
-		this.outputView = new OutputView({model: boxShadow});
-		this.controlView = new ControlView({model: boxShadow});
-	 }
-});
-
-window.App = new AppView;
+new AppView;
 
 });
